1bcf1ab3066caf198662a56b9f566e83a059994c
Marcelo Vieira <marcelodefy15@gmail.com>
2022-08-31 17:09:28 -0300

    req4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -19,6 +20,15 @@ export default class Header extends Component {
       <header data-testid="header-component">
         {user.name !== '' ? <p data-testid="header-user-name">{ user.name }</p>
           : <Loading />}
+        <Link to="/search" data-testid="link-to-search">
+          Search
+        </Link>
+        <Link to="/favorites" data-testid="link-to-favorites">
+          Favorites
+        </Link>
+        <Link to="/profile" data-testid="link-to-profile">
+          Profile
+        </Link>
       </header>
     );
   }
