2f06a41f36658946cd55ec9707a45a31cb29df9b
Marcelo Vieira <marcelodefy15@gmail.com>
2022-08-31 12:47:05 -0300

    req1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 
 export default class Login extends Component {
   render() {
+    const logo = 'https://cdn-icons-png.flaticon.com/512/167/167643.png?w=360'
     return (
       <div data-testid="page-login">
-        Login
+        <img className={ logo } src="" alt="" />
       </div>
     );
   }
